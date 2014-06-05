9cea2f9d90b80c1346e3234cd6ab68fe6bf832a0
Josh Deprez <josh@seikaku.local>
2014-06-05 22:31:00 +1000

    Stopped the regex swallowing trailing commas, and improved decimal place computation

--- simplnum.js
+++ simplnum.js
@@ -5,7 +5,7 @@ function dohtmlreplace(element) {
 	var nodes = element.childNodes;
 	for (var n=0; n<nodes.length; n++) {
 		if (nodes[n].nodeType == Node.ELEMENT_NODE) {
-			var r = new RegExp('\\$(\\s*)([0-9,]+\\.?(\\d*))', 'gi');
+			var r = new RegExp('\\$(\\s*)([0-9,]*\\d(\\.\\d+)?)', 'gi');
 			l = 0
 			nc = ''
 			do {
@@ -20,7 +20,11 @@ function dohtmlreplace(element) {
 					}*/
 					x = x * o;
 					//diff = z - x
-					nu = '<span title="' + m[0] + '">$' + m[1] + x.toFixed(m[3].length) + '</span>'; // + '(' + diff.toFixed(2) + ')';
+					dp = 0
+					if (m[3]) {
+						dp = m[3].length-1					
+					}
+					nu = '<span title="' + m[0] + '">$' + m[1] + x.toFixed(dp) + '</span>'; // + '(' + diff.toFixed(2) + ')';
 					app = nodes[n].innerHTML.substring(l, r.lastIndex-m[0].length) + nu
 					nc = nc + app;
 					l = r.lastIndex
